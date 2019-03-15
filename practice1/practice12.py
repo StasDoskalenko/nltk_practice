8b313a6df18f2510465cb116374de95034fd7c84
Stanislav Doskalenko <doskalenko.s@gmail.com>
2019-03-15 11:41:49 +0200

    westminster abbey text

--- practice1/practice12.py
+++ practice1/practice12.py
@@ -1,10 +1,13 @@
-"""PERCEPTRON TAGGER"""
+"""PERCEPTRON TAGGER and CHUNKING"""
 
 from nltk import word_tokenize, ne_chunk
 from nltk.tag import PerceptronTagger
 from nltk.data import find
 
-sentence = "Mark and Spencer are working at Google."
+sentence = "Westminster Abbey, formally titled the Collegiate Church of Saint Peter at Westminster, is a large, " \
+           "mainly Gothic abbey church in the City of Westminster, London, England, just to the west of the Palace of " \
+           "Westminster. It is one of the United Kingdom's most notable religious buildings and the traditional place " \
+           "of coronation and burial site for English and, later, British monarchs. "
 
 PICKLE = "averaged_perceptron_tagger.pickle"
 AP_MODEL_LOC = 'file:'+str(find('taggers/averaged_perceptron_tagger/'+PICKLE))
